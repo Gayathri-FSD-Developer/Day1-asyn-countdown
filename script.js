7e9c8f5d3ea8b1d66218d264da29ebc0b998372d
Gayathri <150881569+Gayathri-FSD-Developer@users.noreply.github.com>
2023-12-20 23:09:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-document.addEventListener('DOMContentLoaded', () =>
-  (updateTime)
-)
-
+// used setTimer function to call back the another settimer function repeatedly to show the countdown
+// 1000 milesecond = 1 sec
+ 
 let seconds = 10;
-function updateTime()
-{
-  if(seconds>=1)
-      {
-        setTimeout(updateTime,1000)
-        var sec=  document.getElementById("second_col").innerText=seconds;
-        // console.log(seconds);
-        seconds--;
-      }
-      else{
-        var image = document.createElement("img");
-        // image.setAttribute("src","independence.png");
-        // image.setAttribute("width","200");
-        // image.setAttribute("height","300");
-        image.src = "independence.gif";
-    image.alt = "Description of the image";
-    image.width = "700";
-    image.height = "500";
 
-    var sec=  document.getElementById("second_col");
-         sec.appendChild(image);
-        // console.log(image);
-      }
-}
-requestAnimationFrame(updateTime);
+setTimeout(function () {
+    var sec=  document.getElementById("second_col").innerText=seconds;
+  setTimeout(function () {
+    seconds=seconds-1;
+    var sec=  document.getElementById("second_col").innerText=seconds;
+    seconds=seconds-1;
+    setTimeout(function () {
+        seconds=seconds-1;
+        var sec=  document.getElementById("second_col").innerText=seconds;
+
+      setTimeout(function () {
+        seconds=seconds-1;
+        var sec=  document.getElementById("second_col").innerText=seconds;
+
+        setTimeout(function () {
+            seconds=seconds-1;
+            var sec=  document.getElementById("second_col").innerText=seconds;
+
+          setTimeout(function () {
+            seconds=seconds-1;
+            var sec=  document.getElementById("second_col").innerText=seconds;
+        setTimeout(function(){
+            seconds=seconds-1;
+            var sec=  document.getElementById("second_col").innerText=seconds;
+        setTimeout(function(){
+            seconds=seconds-1;
+            var sec=  document.getElementById("second_col").innerText=seconds;
+            setTimeout(function(){
+                seconds=seconds-1;
+                var sec=  document.getElementById("second_col").innerText=seconds;
+                setTimeout(function(){
+                    document.getElementById("second_col").innerText="";
+                    var img=document.createElement("img");
+                    img.src="independence.gif";
+                    img.width = "1000";
+                    img.height = "700";
+                    var sec=  document.getElementById("second_col");
+                    sec.appendChild(img);
+                },1000);
+            },1000);
+        },1000);
+        },1000);
+          }, 1000);
+
+        }, 1000);
+
+      }, 1000);
+
+    }, 1000);
+
+  }, 1000);
+
+}, 0);
